5d5ea41e3bc43aa4e11b302ae8a59d9f1ec66722
Daniel C <333222@gmail.com>
2010-09-11 01:16:28 +0300

    Update setup file (Make it usable)

--- setup.sh
+++ setup.sh
@@ -1,31 +1,49 @@
 #!/bin/bash
 
 source `dirname $0`/setup/script_path.sh # load $SCRIPT_PWD
-source $SCRIPT_PWD/setup/release_name.sh # load the release_name function
 
-shopt -s extglob
-files_to_copy=( )
+link () {
+    local path="$1"
 
-# linking is better than copying and should be used where possible.
-# linked file "updates" automatically after pulling new versions, and simple pull share it with others.
-files_to_link=(
-    # This glob picks all the files from the home dir that begins with . except
-    # of . and ..
-    $scriptDirname/sys-root/home/theosp/.!(.|) 
-)
-shopt -u extglob
+    ln -s $SCRIPT_PWD'/sys-root'$path $path
+}
 
-for file in ${files_to_link[@]}
-do
-    # determine the path for the link
-    target=${file#$scriptDirname}
-    # replace /home/osp with the user's home dir
-    target=${target/\/home\/theosp/~}
-    if release_name $target
-    then
-        ln -s $file $target
-    fi
-done
+copy () {
+    local path="$1"
 
-#chmod +x system-insall-linux-dist.sh
-#exec su -c ./system-insall-linux-dist.sh
+    cp $SCRIPT_PWD'/sys-root'$path $path
+}
+
+sudoCopy () {
+    local path="$1"
+
+    sudo cp $SCRIPT_PWD'/sys-root'$path $path
+}
+
+sudo mkdir -p /usr/share/terminfo/r/
+sudoCopy /usr/share/terminfo/r/rxvt-256color
+
+link /home/theosp/.fluxbox
+link /home/theosp/.vim
+link /home/theosp/.vimrc
+link /home/theosp/.bash_profile
+link /home/theosp/.bashrc
+link /home/theosp/.bashrc-aliases
+link /home/theosp/.bashrc-exports
+link /home/theosp/.bashrc-locale
+link /home/theosp/.screenrc
+link /home/theosp/.Xdefaults
+
+sudo mkdir -p /usr/share/images/wallpapers/
+sudoCopy /usr/share/images/wallpapers/light-fish_1920X1200.jpg
+
+# rm -rf /home/theosp/.fluxbox
+# rm -rf /home/theosp/.vim
+# rm -rf /home/theosp/.bash_profile
+# rm -rf /home/theosp/.bashrc
+# rm -rf /home/theosp/.bashrc-aliases
+# rm -rf /home/theosp/.bashrc-exports
+# rm -rf /home/theosp/.bashrc-locale
+# rm -rf /home/theosp/.screenrc
+# rm -rf /home/theosp/.vimrc
+# rm -rf /home/theosp/.Xdefaults
