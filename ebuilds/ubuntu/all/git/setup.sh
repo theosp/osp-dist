d28d1f93ddfaec193659d1c96337fd46b467594f
Daniel C <333222@gmail.com>
2010-09-21 17:58:29 +0200

    Git ubuntu ebuild - needs libcurl4-gnutls-dev libcurl4-openssl-dev to clone using the http protocol

--- ebuilds/ubuntu/all/git/setup.sh
+++ ebuilds/ubuntu/all/git/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+sudo aptitude install expat curl libcurl4-gnutls-dev libcurl4-openssl-dev
+
 tar -xvjf git-1.7.2.3.tar.bz2
 cd git-1.7.2.3
 ./configure --prefix=/
-make
+make all
 sudo make install
