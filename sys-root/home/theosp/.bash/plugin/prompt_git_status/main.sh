498edde34b55946a40e57528461fc07269cae0d9
Daniel C <333222@gmail.com>
2010-10-16 19:14:33 +0200

    bash: introduce the ~/bash/plugin directory
    Make the code that adds git status to the command prompt a plugin named
    prompt_git_status

---
+++ sys-root/home/theosp/.bash/plugin/prompt_git_status/main.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# When under a git repository this plugin adds the bash prompt indicators about
+# the repository status.
+# 
+# The status includes the current branch and colored circles that have the
+# following meaning:
+# Green: there are staged changes,
+# Yellow: if there are unstaged changes
+# Red: if there are new untracked-yet-unignored files
+#
+# The idea for this script came from:
+# http://briancarper.net/blog/570/git-info-in-your-zsh-prompt
+
+bupler.import style
+bupler.import git
+
+prompt_command_git_status ()
+{
+    local result="$(style.fcolor 4)$(style.bold)$(git.current_branch)"
+    local status=($(git.status))
+
+    if (( ${status[1]} == 1 )) || (( ${status[2]} == 2 )) || (( ${status[3]} == 3 )) || (( ${status[4]} == 4 )) || (( ${status[5]} == 5 ))
+    then
+        #echo "staged changes"
+        result+="$(style.fcolor 2)$(style.bold)●$(style.reset)"
+    fi
+    if (( ${status[6]} == 1 )) || (( ${status[7]} == 1 ))
+    then
+        #echo "unstaged changes"
+        result+="$(style.fcolor 3)$(style.bold)●$(style.reset)"
+    fi
+    if (( ${status[0]} == 1 ))
+    then
+        #echo "untracked changes"
+        result+="$(style.fcolor 1)$(style.bold)●$(style.reset)"
+    fi
+
+    echo "$result"
+}
+
+if [ "$TERM" != 'dumb' ] && [ -n "$BASH" ] && [ -n "$PS1" ]
+then
+    # The extra line after <<EOF is for cases where there is already
+    # PROMPT_COMMAND that doesn't ends with ;
+    PROMPT_COMMAND+="$(cat <<EOF
+
+        if git.is_rep > /dev/null
+        then
+            export PS1="\$(style.fcolor 2)\$(style.bold)\u@\h \$(style.fcolor 4)\$(style.bold)\W [\$(style.reset)\$(prompt_command_git_status)\$(style.fcolor 4)\$(style.bold)] \$ \$(style.reset)"
+        else
+            if [ "\$(/usr/bin/whoami)" = 'root' ]
+            then
+                export PS1='\[\033[01;31m\]\h \[\033[01;34m\]\W \$ \[\033[00m\]'
+            else
+                export PS1='\[\033[01;32m\]\u@\h \[\033[01;34m\]\W \$ \[\033[00m\]'
+            fi
+        fi
+EOF
+    )"
+fi
+# vim:ft=bash:
--- sys-root/home/theosp/.bashrc
+++ sys-root/home/theosp/.bashrc
@@ -84,53 +84,10 @@ case "$TERM" in
 esac
 
 
-# When under a git repository this function adds the bash prompt indicators about
-# the repository status.
-# 
-# The status includes the current branch and colored circles that have the
-# following meaning:
-# Green: there are staged changes,
-# Yellow: if there are unstaged changes
-# Red: if there are new untracked-yet-unignored files
-#
-# The idea for this script came from:
-# http://briancarper.net/blog/570/git-info-in-your-zsh-prompt
-bupler.import style
-bupler.import git
-prompt_command_git_status ()
-{
-    local result="$(style.fcolor 4)$(style.bold)$(git.current_branch)"
-    local status=($(git.status))
-
-    if (( ${status[1]} == 1 )) || (( ${status[2]} == 2 )) || (( ${status[3]} == 3 )) || (( ${status[4]} == 4 )) || (( ${status[5]} == 5 ))
-    then
-        #echo "staged changes"
-        result+="$(style.fcolor 2)$(style.bold)●$(style.reset)"
-    fi
-    if (( ${status[6]} == 1 )) || (( ${status[7]} == 1 ))
-    then
-        #echo "unstaged changes"
-        result+="$(style.fcolor 3)$(style.bold)●$(style.reset)"
-    fi
-    if (( ${status[0]} == 1 ))
-    then
-        #echo "untracked changes"
-        result+="$(style.fcolor 1)$(style.bold)●$(style.reset)"
-    fi
-
-    echo "$result"
-}
-
-export original_ps="$PS1"
-PROMPT_COMMAND+="$(cat <<EOF
-
-    if git.is_rep > /dev/null
-    then
-        export PS1="\$(style.fcolor 2)\$(style.bold)\u@\h \$(style.fcolor 4)\$(style.bold)\W [\$(style.reset)\$(prompt_command_git_status)\$(style.fcolor 4)\$(style.bold)] \$ \$(style.reset)"
-    else
-        export PS1="\$original_ps"
-    fi
-EOF
-)"
+# load plugins
+for i in ~/.bash/plugin/*
+do
+    . "$i/main.sh"
+done
 
 clear
