cf57848015a0f832c540b1fc57b3ec945cb75d01
Daniel C <333222@gmail.com>
2010-11-08 11:14:55 +0000

    Add ubuntu ebuild for python PIL

---
+++ ebuilds/ubuntu/all/python_pil.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo aptitude install python-dev
+
+wget http://effbot.org/downloads/Imaging-1.1.7.tar.gz
+
+tar -xvzf Imaging-1.1.7.tar.gz
+
+cd Imaging-1.1.7
+
+sudo python setup.py install
