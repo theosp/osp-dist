10b80f61d423ea2b6c0dd3d1fd88a07e67d279ba
Daniel C <333222@gmail.com>
2010-12-05 10:44:20 +0200

    Add an ubuntu ebuild for lesscss (lessc) and add filetype detection and syntax support for .less

---
+++ ebuilds/ubuntu/all/lessc/lessc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo aptitude install rubygems1.8
+
+sudo gem install less
+
+sudo ln -s /var/lib/gems/1.8/bin/lessc /usr/local/bin
---
+++ sys-root/home/theosp/.vim/ftdetect/mine.vim
@@ -0,0 +1,5 @@
+" For help check :h new-filetype
+
+au BufRead,BufNewFile *.less		set filetype=lessc
+
+" vim:ft=vim:
