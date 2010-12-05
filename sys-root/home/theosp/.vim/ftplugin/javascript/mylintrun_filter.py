87220ec7eef2b5dc3de50733808f87e297d9ae97
Daniel C <333222@gmail.com>
2010-12-05 10:49:28 +0200

    vim jslint: filter django's {% option %} tags before calling jslint

--- sys-root/home/theosp/.vim/ftplugin/javascript/mylintrun_filter.py
+++ sys-root/home/theosp/.vim/ftplugin/javascript/mylintrun_filter.py
@@ -10,6 +10,8 @@ source = re.sub(r'{{.+?}}', '', source) # remove vars
 
 source = re.sub(r'{% comment %}', '/*', source)
 source = re.sub(r'{% endcomment %}', '*/', source)
+
+source = re.sub(r'{%\s*option.*?%}', "''", source)
 # }}}
 
 print source,
