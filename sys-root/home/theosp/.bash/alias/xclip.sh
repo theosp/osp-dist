8fe9083c1811f0927552f856bcee2336c5f165dc
Daniel C <333222@gmail.com>
2010-12-29 17:06:31 +0200

    Add alias for the xclip command copy+/* paste+/*

---
+++ sys-root/home/theosp/.bash/alias/xclip.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+alias copy+='xclip -selection clipboard -i'
+alias copy*='xclip -selection primary -i'
+alias paste+='xclip -selection clipboard -o'
+alias paste*='xclip -selection primary -o'
