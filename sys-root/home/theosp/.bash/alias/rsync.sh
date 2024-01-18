80a0ae0295cd83b78451946f92a8e8e8354180be
Daniel C <daniel@meteorspark.com>
2024-01-19 00:12:38 +0700

    Update my rsync scripts

--- sys-root/home/theosp/.bash/alias/rsync.sh
+++ sys-root/home/theosp/.bash/alias/rsync.sh
@@ -28,24 +28,35 @@ rsync-my-preferences () {
         return 1
     fi
 
-    echo "Note that for dry-run you can call:"
     echo ""
-    echo "$ rsync-my-preferences-dry-run $@"
+    echo "Running the dry-run... $ rsync-my-preferences-dry-run $@"
+    echo ">>> The dry-run has a total output of"$(style.info)$(rsync-my-preferences-dry-run "$@" | wc -l)$(style.reset)" lines, do you want to see them?"
     echo ""
 
-    echo "This will run the command: $"
+    read -p "Show the dry-run output? (Y/n) " -n 1 -r
+    if [[ ! "$REPLY" =~ ^[Nn]$ ]]
+    then
+        echo ""
+        echo ">>> Running the dry-run:"
+        echo ""
+        rsync-my-preferences-dry-run "$@"
+        echo ""
+    fi
+
+    echo 
+    echo "If you proceed the following command will run: "
     echo 
     echo "$ $command $@"
     echo
 
-
-    read -p "Are you sure you want to proceed? (y/n) " -n 1 -r
-    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    read -p "Are you sure you want to proceed? (y/N) " -n 1 -r
+    if [[ ! "$REPLY" =~ ^[Yy]$ ]]
     then
         echo "Aborting."
         return 1
     fi
 
+    echo
     echo "Running in 3 seconds..."
     sleep 3
     $command "$@"
