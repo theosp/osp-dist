5a139bbb0ca9fc464905eef23143ccf57a20c102
Daniel C <daniel@meteorspark.com>
2024-01-19 23:59:55 +0700

    Add find-list-files-count-and-size

--- sys-root/home/theosp/.bash/alias/find.sh
+++ sys-root/home/theosp/.bash/alias/find.sh
@@ -2,4 +2,15 @@
 
 alias find-delete-ds-store='find . -name '.DS_Store' -type f -delete' 
 
+# A function that prints for each of the folders in the current cwd the number of files in its entire folder tree
+find-list-files-count-and-size() {
+    echo -e "Folder\t\t\tSize\t\tFiles"
+    for file in *; do
+        if [ -d "$file" ]; then
+            du_result_arr=( $(du -sh "$file") )
+            echo $file$'\t\t'${du_result_arr[0]}$'\t\t'$(find "$file" -type f | wc -l)
+        fi
+    done
+}
+
 # vim:ft=bash:
