f7bcb5bc07ae859d21f04367329c12f6e47479e6
Daniel C <daniel@meteorspark.com>
2024-02-02 09:08:32 +0700

    Improve sha256sum utils

--- sys-root/home/theosp/.bash/alias/sha256sum.sh
+++ sys-root/home/theosp/.bash/alias/sha256sum.sh
@@ -1,15 +1,30 @@
 alias sha256sum="shasum -a 256"
 
-hashes_file="hashes.txt"
+hashes_file="sha256-hashes.txt"
 
 sha256sum-rec () {
-    find $1 -type f -exec sha256sum {} \;
+    local path="$1"
+    shift
+    local find_extra_args=( "$@" )
+
+    # if not provided a path, use the current directory
+    if [ -z "$path" ]; then
+        path="."
+    fi
+
+    # If not a directory, print an error
+    if [ ! -d "$path" ]; then
+        echo "Error: $path is not a directory"
+        return 1
+    fi
+
+    platformFind "$path" -type f "${find_extra_args[@]}" -exec sha256sum {} \;
 }
 
 sha256sum-produce-hashes-file () {
     # If an hashes file already exists, check with the user if they want to overwrite it
     if [ -f "$hashes_file" ]; then
-        read -p "Hashes file already exists. Overwrite? [y/N] " -n 1 -r
+        read -p "Hashes file \`$hashes_file\` already exists. Overwrite? [y/N] " -n 1 -r
         echo
         if [[ ! $REPLY =~ ^[Yy]$ ]]; then
             echo "Aborting"
@@ -17,7 +32,9 @@ sha256sum-produce-hashes-file () {
         fi
     fi
 
-    sha256sum-rec . > "$hashes_file"
+    sha256sum-rec . -and ! -name "$hashes_file" | tee "$hashes_file"
+
+    echo "Hashes file \`$hashes_file\` created"
 }
 
 sha256sum-verify-hashes-file () {
